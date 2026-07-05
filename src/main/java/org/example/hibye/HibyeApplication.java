289e80b54682d3ded4ef937e9330d8d659996617
Thulakshana <thulakshanadissanayake13@gmail.com>
2026-07-05 14:44:50 +0530

    initial commit

---
+++ src/main/java/org/example/hibye/HibyeApplication.java
@@ -0,0 +1,13 @@
+package org.example.hibye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibyeApplication.class, args);
+    }
+
+}
